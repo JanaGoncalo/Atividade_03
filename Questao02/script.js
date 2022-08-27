b0b71ef4447abd7117ab04d5e832d70d5c665c74
Janaína Gonçalo <103140022+JanaGoncalo@users.noreply.github.com>
2022-08-27 12:50:12 -0300

    Update script.js

--- Questao02/script.js
+++ Questao02/script.js
@@ -8,22 +8,27 @@ Como retorno deve ser passado um vetor que tem 2 valores um para cada possível 
 Caso o delta seja negativo, retorne, ao invés do vetor, um string com a frase: “Delta é negativo”.
 */
 
-Bhaskara = (ax2, bx, c) => {
-  let delta = bx * bx - 4 * ax2 * c
+Formula = () => {
+  const A = document.getElementById('A')
+  const B = document.getElementById('B')
+  const C = document.getElementById('C')
+  const result = document.getElementById('result')
+  result.innerHTML = ''
 
-  if (delta < 0) {
-    console.log(delta)
-    return console.log('Delta é Negativo.')
+  Bhaskara = (ax2, bx, c) => {
+    let delta = bx * bx - 4 * ax2 * c
+
+    if (delta < 0) {
+      return (result.innerHTML = `Delta é negativo: ${delta}`)
+    }
+
+    let raiz = Math.sqrt(delta)
+
+    let x1 = (-bx + raiz) / (2 * ax2)
+    let x2 = (-bx - raiz) / (2 * ax2)
+
+    let bhaskara = [x1, x2]
+    return (result.innerHTML = `O valor de x1 e x2 são respectivamente : ${bhaskara}`)
   }
-
-  let raiz = Math.sqrt(delta)
-
-  let x1 = (-bx + raiz) / (2 * ax2)
-  let x2 = (-bx - raiz) / (2 * ax2)
-
-  let bhaskara = [x1, x2]
-
-  console.log(`O valor de x1 e x2 são: ${baskara}`)
+  Bhaskara(A.value, B.value, C.value)
 }
-
-Bhaskara(3, -5, 12)
